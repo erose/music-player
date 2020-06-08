9be500efd6ca2ec37a18c5ff5d916a92082dd20f
Eli Rose <really.eli@gmail.com>
2020-06-08 10:34:18 -0400

    Add name of currently uploading file to upload.sh.

--- upload_music.sh
+++ upload_music.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for filename in "$@"; do
+  echo "Uploading $filename..."
   s3name=s3://elis-music/$(ruby -e "puts ARGV[0].gsub('#', '/')" "$filename")
   aws s3 mv "$filename" "$s3name"
 done
