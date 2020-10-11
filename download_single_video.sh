ecf9873f7824247b0fcb82831bda808cdfa83c30
Eli Rose <really.eli@gmail.com>
2020-10-10 23:58:03 -0700

    Rename download script and allow escaping # in uploaded song names.

--- download_single_video.sh
+++
@@ -1,1 +0,0 @@
-youtube-dl --extract-audio --audio-format mp3 --output 'output.%(ext)s' "$1"
--- upload_music.sh
+++ upload_music.sh
@@ -2,6 +2,6 @@
 
 for filename in "$@"; do
   echo "Uploading $filename..."
-  s3name=s3://elis-music/$(ruby -e "puts ARGV[0].gsub('#', '/')" "$filename")
+  s3name=s3://elis-music/$(ruby -e "puts ARGV[0].gsub(/(?<!\\\)#/, '/')" "$filename") # negative lookbehind to implement escaping; means we can use '\#' as a real '#'
   aws s3 mv "$filename" "$s3name"
 done
